90e5ca2a705725cd2da4daa0bbd6319c5fd78acb
malek bokri <malek.bokri@gmail.com>
2024-11-22 00:31:00 +0100

    finish member details

--- Client/src/app/members/member-detail/member-detail.component.ts
+++ Client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/Member';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [],
+  imports: [GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -14,17 +15,28 @@ export class MemberDetailComponent implements OnInit {
   member? :Member ;
   rout = inject(ActivatedRoute);
   membersService=inject(MembersService);
+  images: GalleryItem[]=[];
   ngOnInit(): void {
       this.loadMember();
+      console.log(this.images);
+
   }
   loadMember(){
     const username=this.rout.snapshot.paramMap.get("username");
     if(username){
       this.membersService.getMember(username)
                   .subscribe({
-                    next : response=>this.member=response
+                    next : response=>{this.member=response;
+                      this.loadImages(response);
+                    }
                   })
     }
+  }
+  loadImages(member:Member){
+    member.photos?.forEach(element =>{ 
+      this.images.push( new ImageItem({ src: `${element.url}`, thumb: `${element.url}` }));
+      console.log(this.images);}
+    );
   }
 
 }
