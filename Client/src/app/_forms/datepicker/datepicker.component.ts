f9787e2b443255d747dc5a9ebd8e89d7fae5e375
malek bokri <malek.bokri@gmail.com>
2024-12-03 19:51:28 +0100

    finish registration

---
+++ Client/src/app/_forms/datepicker/datepicker.component.ts
@@ -0,0 +1,32 @@
+import { NgIf } from '@angular/common';
+import { Component, input, Self } from '@angular/core';
+import { FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'app-datepicker',
+  standalone: true,
+  imports: [BsDatepickerModule,ReactiveFormsModule,NgIf],
+  templateUrl: './datepicker.component.html',
+  styleUrl: './datepicker.component.css'
+})
+export class DatepickerComponent {
+  label = input<string>('');
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this
+  }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl
+  }
+}
